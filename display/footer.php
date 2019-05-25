54a29d9bf11ae8baf0be3b40508a076886d42f67
GuiiiuG <vjpking5913@gmail.com>
2019-05-25 15:46:52 +0700

    Footer

---
+++ display/footer.php
@@ -0,0 +1,38 @@
+<footer class="motopress-wrapper footer" >
+<div class="container">
+<div class="row">
+<div class="span12" data-motopress-wrapper-file="wrapper/wrapper-footer.php" data-motopress-wrapper-type="footer" data-motopress-id="562efa5252b2e">
+<div class="row footer-widgets">
+<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-1">
+</div>
+<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-2">
+</div>
+<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-3">
+</div>
+<div class="span3" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-4">
+</div>
+</div>
+<div class="row copyright">
+<div class="span12 bot3" data-motopress-type="static" data-motopress-static-file="static/static-footer-text.php">
+<nav class="nav footer-nav">
+<ul id="menu-footer-menu" class="menu"><li id="menu-item-1816" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-203 current_page_item menu-item-1816"><a href="index.php">TRANG CHỦ </a></li>
+<li id="menu-item-1821" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1821"><a href="?page=tintuc">TIN TỨC</a></li>
+<li id="menu-item-1819" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1819"><a href="?page=sanpham">SHOP</a></li>
+<li id="menu-item-1817" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1817"><a href="?page=contact">
+    Liên hệ
+</a></li>
+</ul> </nav>
+</div>
+<div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-footer-nav.php">
+<div id="footer-text" class="footer-text">
+
+ 
+</div> </div>
+</div> </div>
+</div>
+</div>
+    <p style="color:#009FFF" class="text-center">Desigh by : Mai Hồng Ngự, Ngô Trường Hải, Võ Xuân Hậu</p>
+</footer>
+ 
+</body>
+</html>
